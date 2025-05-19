bdeabb3511b806dcb58b5f86e1d3c1dbcaab2cfd
Juan J Ramírez Z <juanjramirezz@mac.lan>
2025-05-19 16:02:50 -0500

    Taller fullstack casi completo :)

---
+++ src/app/api/firestore/route.js
@@ -0,0 +1,23 @@
+import { getFirestore, collection, doc, setDoc, getDocs } from "firebase/firestore";
+import app from "../../../../firebase/firebase";
+import { NextResponse } from "next/server";
+
+const firestore = getFirestore(app);
+
+export async function POST(req) {
+    const { data } = await req.json();
+    try {
+        const docRef = doc(collection(firestore, "cartas"));
+        const id = docRef.id;
+        await setDoc(docRef, { ...data, id });
+        return NextResponse.json({ id: id, ...data });
+    } catch (error) {
+        return NextResponse.json({ error: "Failed to add document " + error });
+    }
+}
+
+export async function GET() {
+    const snapshot = await getDocs(collection(firestore, "cartas"));
+    const data = snapshot.docs.map((doc) => ({ ...doc.data() }));
+    return NextResponse.json(data);
+}
